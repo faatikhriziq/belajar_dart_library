90b2aaeded02a1ca74d51df8a900f1ddf227bb68
faatikhriziq <faatikhriziq@gmail.com>
2023-01-21 05:49:48 +0700

    belajar dart library

---
+++ example/belajar_dart_library_example.dart
@@ -0,0 +1,7 @@
+import 'package:belajar_dart_library/hello.dart';
+
+
+void main(){
+  var cutomer = Customer();
+  print(sayHello("Muhammad Ali Faatikh Riziq"));
+}
---
+++ lib/src/customer.dart
@@ -0,0 +1,4 @@
+class Customer{}
+class Product{}
+class Category{}
+
